1a839a79fb1f5ac15633ccb2aef3b95eed6db78c
Cesar McFarlin <cesarmcfarlin@cesars-mbp.lan>
2018-06-12 21:57:51 -0600

    Intial  Setup
    HTML, CSS, IMAGES, JS intial frame work ands tart

---
+++ assets/javascript/game.js
@@ -0,0 +1,27 @@
+// Set Variables
+// gemOne - gemFour  (random numbers to be set 1-10)
+// winningNumber (random number 30-150)
+// runningNumber (users score added per the buttons)
+
+var gem1 = Math.floor(Math.random()*10+1);
+var gem2 = Math.floor(Math.random()*10+1);
+var gem3 = Math.floor(Math.random()*10+1);
+var gem4 = Math.floor(Math.random()*10+1);
+var winningNumber = Math.floor(Math.random()*131+29);
+var runningNumber = 0;
+
+
+// winScore & loseScore - if/else statment 
+
+// Document Ready - Assign winningNumber and 4 gem numbers
+
+// onclick - add assigned interger to runningNumber
+
+// push var to doc
+
+// if runningNumber === winningNumber set win  - tally win
+
+// else runningNumber > winningNumber set lose - tally lose
+
+// reset game - reassign numbers and reiterate game again
+
